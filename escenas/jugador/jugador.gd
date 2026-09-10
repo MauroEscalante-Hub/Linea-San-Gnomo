7dd26a7066c9367aac22b753fafbed690fd6226c
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-09 23:01:54 -0300

    kick off

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 var velocidad_minima = 1.0
 @export var ACELERACION = 20.0
 @export var FRENO = 10.0
-@export var Giro = 2.0
+@export var Girar = 2.0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,9 +14,9 @@ func _physics_process(delta):
 
 # Rotación del "auto" con izquierda/derecha.
 	if Input.is_action_pressed("izquierda"):
-		rotate_y(Giro * delta)
+		rotate_y(Girar * delta)
 	if Input.is_action_pressed("derecha"):
-		rotate_y(-Giro * delta)
+		rotate_y(-Girar * delta)
 
 	# Si está apretando "frenar", decelera gradualmente hasta 0 y no procesa más movimiento.
 	if Input.is_action_pressed("frenar"):
@@ -34,3 +34,18 @@ func _physics_process(delta):
 	velocity.z = move_toward(velocity.z, direccion.z * velocidad_objetivo, ACELERACION * delta)
 
 	move_and_slide()
+
+	#version sin tocar nada frena 
+	#var avanzando := Input.is_action_pressed("avanzar")
+	#var direction := Vector3.ZERO
+	#if avanzando:
+		#direction = -transform.basis.z  # el "adelante" del auto según su rotación actual
+#
+	#if direction:
+		#velocity.x = move_toward(velocity.x, direction.x * SPEED, ACELERACION * delta)
+		#velocity.z = move_toward(velocity.z, direction.z * SPEED, ACELERACION * delta)
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, ACELERACION * delta)
+		#velocity.z = move_toward(velocity.z, 0, ACELERACION * delta)
+#
+	#move_and_slide()
