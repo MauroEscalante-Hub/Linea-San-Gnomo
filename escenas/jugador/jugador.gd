88d829436c4cb47add2ea3ae3cb5e954f2336f46
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-15 23:00:29 -0300

    ultimo

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -4,6 +4,10 @@ extends VehicleBody3D
 @export var max_steer := 1.1
 @export var velocidad_de_giro := 10
 @export var engine_power := 100
+@onready var centro_de_masa := $"centro de masa"
+
+func _ready():
+	center_of_mass = centro_de_masa.position
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
