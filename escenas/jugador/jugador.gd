1ab47d23ec58aa0fb88454d18199711368c827e4
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-16 20:02:59 -0300

    aun resolviendo problemas
    aún no puedo resolver que quede pegado a la pared y que se maneje

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -7,24 +7,38 @@ extends VehicleBody3D
 @onready var centro_de_masa := $"centro de masa"
 @onready var detector_derecho := $RayCast_derecha
 @onready var detector_izquierdo := $RayCast_izquierda
+var pegado_a_pared :bool = false
+
 func _ready():
 	center_of_mass = centro_de_masa.position
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
 	engine_force = Input.get_axis("frenar", "avanzar") * engine_power
+	print("frente auto: ", -transform.basis.z)
 	
-	if detector_derecho.is_colliding():
-		#print("se detecto algo, soy derecho")
+	if pegado_a_pared:
+		print("ESTOY PEGADO")
+	
+	if detector_derecho.is_colliding() and !pegado_a_pared:
 		var normal = detector_derecho.get_collision_normal()
-		var direccion_pared = normal.cross(Vector3.UP)
+		var direccion = normal.cross(Vector3.UP)
+		direccion.y = 0
+		direccion = direccion.normalized()
+		var nuevo_basis = Basis.looking_at(direccion, normal)
+		global_transform.basis = nuevo_basis
+		var hacia_pared = -normal
+		apply_central_force(hacia_pared * 100.0)
+		pegado_a_pared = true
 		
-		print("[velocidad] ",linear_velocity)
-		print("[direccion pared] ", direccion_pared)
+	if detector_izquierdo.is_colliding()and !pegado_a_pared:
+		var normal = detector_izquierdo.get_collision_normal()
+		var direccion = -normal.cross(Vector3.UP)
+		direccion.y = 0
+		direccion = direccion.normalized()
+		var nuevo_basis = Basis.looking_at(direccion, normal)
+		global_transform.basis = nuevo_basis
+		var hacia_pared = -normal
+		apply_central_force(hacia_pared * 100.0)
+		pegado_a_pared = true
 	
-	if detector_izquierdo.is_colliding():
-		#print("se detecto algo, soy izquierdo")
-		#var normal = detector_izquierdo.get_collision_normal()
-		#var direccion = normal.cross(Vector3.UP)
-		#print(direccion)
-		print("[izquierdo] ",linear_velocity)
