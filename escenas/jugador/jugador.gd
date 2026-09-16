a95c3cbd047580cef5dcdef00e6c6f939e74dc46
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-15 22:43:52 -0300

    kick off

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -1,9 +1,10 @@
 class_name Coche
 extends VehicleBody3D
 
-@export var max_steer := 1.0
-@export var engine_power := 300
+@export var max_steer := 1.1
+@export var velocidad_de_giro := 10
+@export var engine_power := 100
 
 func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("derecha","izquierda") * max_steer, delta * 10)
+	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
 	engine_force = Input.get_axis("frenar", "avanzar") * engine_power
