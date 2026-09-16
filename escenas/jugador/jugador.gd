2b020b846cf0133ee440009071282c85b24380a7
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-16 17:19:48 -0300

    analizando con los raycast si detecta o no las paredes
    mas que nada realizar pruebas con las paredes del escenario grayboxing anterior

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -5,10 +5,20 @@ extends VehicleBody3D
 @export var velocidad_de_giro := 10
 @export var engine_power := 100
 @onready var centro_de_masa := $"centro de masa"
-
+@onready var detector_derecho := $RayCast_derecha
+@onready var detector_izquierdo := $RayCast_izquierda
 func _ready():
 	center_of_mass = centro_de_masa.position
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
 	engine_force = Input.get_axis("frenar", "avanzar") * engine_power
+	
+	if detector_derecho.is_colliding():
+		print("se detecto algo, soy derecho")
+		var normal = detector_derecho.get_collision_normal()
+		print(normal)
+	if detector_izquierdo.is_colliding():
+		print("se detecto algo, soy izquierdo")
+		var normal = detector_izquierdo.get_collision_normal()
+		print(normal)
