97512dfc3778c7eb70b84bcf8321a0e86843d5de
MauroEscalante-Hub <ms.fangamesclasicos@gmail.com>
2026-09-17 15:25:55 -0300

    Balas_agregadas

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -17,6 +17,36 @@ func _ready():
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
 	engine_force = Input.get_axis("frenar", "avanzar") * engine_power
+	if detector_derecho.is_colliding() and !pegado_a_pared:
+		var normal = detector_derecho.get_collision_normal()
+		var punto = detector_derecho.get_collision_point()
+		var direccion = normal.cross(Vector3.UP)
+		direccion.y = 0
+		direccion = direccion.normalized()
+		var nuevo_basis = Basis.looking_at(direccion, normal)
+		global_transform.basis = nuevo_basis
+		
+		var distancia = global_position.distance_to(punto)
+		var fuerza_pared = distancia * 300.0
+
+		apply_central_force(-normal * fuerza_pared)
+		pegado_a_pared = true
+		
+	if detector_izquierdo.is_colliding()and !pegado_a_pared:
+		var normal = detector_izquierdo.get_collision_normal()
+		var punto = detector_izquierdo.get_collision_point()
+		
+		var direccion = -normal.cross(Vector3.UP)
+		direccion.y = 0
+		direccion = direccion.normalized()
+		var nuevo_basis = Basis.looking_at(direccion, normal)
+		global_transform.basis = nuevo_basis
+		var distancia = global_position.distance_to(punto)
+		var fuerza_pared = distancia * 300.0
+		
+		apply_central_force(normal * fuerza_pared)
+		pegado_a_pared = true
+	
 
 func  _input(event: InputEvent) -> void:
 	if event.is_action_pressed("dispararbalas"):
@@ -28,3 +58,4 @@ func disparo():
 	$arma.global_position = balita.global_position
 	balita.direccion = -transform.basis.z.normalized()
 	add_child(balita)
+	
