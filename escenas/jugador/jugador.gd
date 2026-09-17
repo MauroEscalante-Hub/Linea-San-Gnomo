82ac9ce14409d6db895afe51e7e5745d1ed4bc1a
YaninamiLagros <mily.ostay@gmail.com>
2026-09-16 22:57:43 -0300

    Instanciar balas al jugador

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -6,9 +6,22 @@ extends VehicleBody3D
 @export var engine_power := 100
 @onready var centro_de_masa := $"centro de masa"
 
+@export var bala : PackedScene
+
 func _ready():
 	center_of_mass = centro_de_masa.position
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("derecha","izquierda") * max_steer, velocidad_de_giro * delta)
 	engine_force = Input.get_axis("frenar", "avanzar") * engine_power
+
+func  _input(event: InputEvent) -> void:
+	if event.is_action_pressed("dispararbalas"):
+		disparo()
+		
+		
+func disparo():
+	var balita = bala.instantiate()
+	balita.global_position = $arma.global_position
+	balita.direccion = -transform.basis.z.normalized()
+	add_child(balita)
