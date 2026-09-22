9f03925beca29cf7c38718f2349952c958af4ff5
ArielFigu <santiagoarielf27@gmail.com>
2026-09-22 14:54:07 -0300

    tren  se mueve een el recorrido del nivel planteado
    el tren circula por el riel hasta el final  del recorrido

--- Escenas/path_follow_3d.gd
+++ Escenas/path_follow_3d.gd
@@ -1,8 +1,7 @@
 extends PathFollow3D
 
-@export var speed :float=0.0089
+@export var speed :float=0.0049
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress_ratio += delta*speed
