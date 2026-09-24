8d205c4350078c2e4d7bc90661213065ad424dd0
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-09-23 21:03:05 -0300

    Añadimos la señal
    señal para corroborar que estamos para cambiar de auto

--- escenas/componentes/deteccion_de_vehiculo.gd
+++ escenas/componentes/deteccion_de_vehiculo.gd
@@ -2,13 +2,13 @@ class_name DeteccionDeVehiculo
 extends Node3D
 
 @export var auto_actual := VehicleBody3D
-
-
+signal listo_para_cambiar
 
 func _on_area_3d_body_entered(body):
 	if body is VehicleBody3D and body != get_parent():
 		auto_actual = body
 		print("Hay un auto adentro del area: ", auto_actual.name)
+		listo_para_cambiar.emit()
 
 func _on_area_3d_body_exited(body):
 	if body == auto_actual:
