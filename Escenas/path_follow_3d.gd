9bf275b9d3f02cb32215c38a75507505c44c776d
ArielFigu <santiagoarielf27@gmail.com>
2026-09-24 16:21:56 -0300

    El tren desacelera si el auto esta muy lejos
    El tren acelera si esta muy junto al jugador  desacelera si la distancia es mayor a 15, pero si el auto esta muy delante, el tren sigue lento

--- Escenas/path_follow_3d.gd
+++ Escenas/path_follow_3d.gd
@@ -1,7 +1,16 @@
 extends PathFollow3D
 
-@export var speed :float=0.0049
+@export var VelocidadNormal :float=0.0199
+@export var Desaceleracion:float = 0.6
+@export var DistanciaMax: int=15
+@onready var jugador: Coche = $"../../jugador"
+
+@onready var Locomotora: VehicleBody3D = $VehicleBody3D
 
 
-func _process(delta: float) -> void:
-	progress_ratio += delta*speed
+func _process(delta: float):
+	var distancia=Locomotora.global_position.distance_to(jugador.global_position)
+	if  distancia >=  DistanciaMax:
+		progress_ratio += (VelocidadNormal-(VelocidadNormal*Desaceleracion))*delta
+	else:
+		progress_ratio += VelocidadNormal*delta
